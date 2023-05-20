6dd753614cccafcbce3b678055bb8195e49a0399
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2023-05-20 15:04:09 +0400

    update

--- project/index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("myID").addEventListener("mouseover", function() {
-    document.getElementById("myID").style.backgroundColor = "green";
-});
-    
-document.getElementById("myID").addEventListener("mouseout", function() {
-    document.getElementById("myID").style.backgroundColor = "red";
-});
